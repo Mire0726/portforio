203c50aba498cae212b47f125bf8d96063bedc91
sumire <violeta.jin.726@gmail.com>
2023-03-19 23:13:05 +0900

    ページ遷移

--- 8080server/app.js
+++ 8080server/app.js
@@ -7,10 +7,16 @@ app.listen(8080, () => {
   console.log("Start Server!");
 });
 
-app.use(express.static("public"));
+app.use(express.static("8080server"));
 
 app.use("/", (req, res) => {
   console.log("Hello, express");
   res.sendFile(__dirname + "/public/index.html");
   res.sendFile(__dirname + "/public/style.css");
+});
+
+app.use("//Users/inouesumire/Downloads/portforio/8080server/works/works.html", (req, res) => {
+  console.log("Hello, express");
+  res.sendFile(__dirname + "/works/works.html");
+  res.sendFile(__dirname + "/works/works.css");
 });
